6a639c2fa1642a22d2bcab0615bc5632be265529
Edu Lima <eduardo.detominilima@metronom.com>
2019-12-18 20:34:38 +0100

    Testing small change

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome!
+            Fibonacci Calculator
         </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
